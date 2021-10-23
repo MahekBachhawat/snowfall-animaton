df2d6fd97b0644d964b98216ad56299ef6b0d59e
MahekBachhawat <82311964+MahekBachhawat@users.noreply.github.com>
2021-10-23 13:17:33 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,79 +1,77 @@
-var Engine = Matter.Engine,
-  World = Matter.World,
-  Events = Matter.Events,
-  Bodies = Matter.Bodies;
- 
-var particles = [];
-var plinkos = [];
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies =  Matter.Bodies;
+const Constraint = Matter.Constraint;
+var bg,ground,gimg;
+var runner,runnerImg;
+var ice=[];
+var maxSnow=100;
 
-var divisionHeight=300;
-var score =0;
-function setup() {
-  createCanvas(800, 800);
-  engine = Engine.create();
-  world = engine.world;
-  ground = new Ground(width/2,height,width,20);
-
-
-   for (var k = 0; k <=width; k = k + 80) {
-     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
-   }
-
-
-    for (var j = 75; j <=width; j=j+50) 
-    {
-    
-       plinkos.push(new Plinko(j,75));
-    }
-
-    for (var j = 50; j <=width-10; j=j+50) 
-    {
-    
-       plinkos.push(new Plinko(j,175));
-    }
-
-     for (var j = 75; j <=width; j=j+50) 
-    {
-    
-       plinkos.push(new Plinko(j,275));
-    }
-
-     for (var j = 50; j <=width-10; j=j+50) 
-    {
-    
-       plinkos.push(new Plinko(j,375));
-    }
-
-    
-
-    
+function preload(){
+  bg=loadImage("snow2.jpg");
+  gimg=loadImage("ground.PNG");
+  runnerImg=loadAnimation("sc1.PNG","sc2.PNG","sc3.PNG","sc4.PNG","sc5.PNG","sc6.PNG","sc7.PNG","sc8.PNG","sc9.PNG","sc10.PNG","sc11.PNG","sc12.PNG")
 }
- 
 
+function setup() {
+  createCanvas(1300,600);
+  
+  engine=Engine.create();
+  world= engine.world;
+  
+
+
+ground=createSprite(650,670);
+ground.addImage(gimg);
+ground.scale=3.2;
+ground.velocityX=-10;
+
+runner=createSprite(150,480);
+runner.addAnimation("runner",runnerImg)
+runner.scale=1.1;
+runner.velocityX=2;
+runner.setCollider("rectangle",15, -20,100,180) 
+
+if(frameCount % 275 === 0){
+  for(var i=0; i<maxSnow; i++){
+  ice.push(new Snow(random(0,1350), random(0,50)));
+  }
+  }
+
+
+}
 
 function draw() {
-  background("black");
-  textSize(20)
- //text("Score : "+score,20,30);
+  background(bg);  
   Engine.update(engine);
- 
+
+  runner.collide(ground);
+
+  if(ground.x < 530){
+    ground.x=600;
+  }
+
+  if(runner.x > 1200){
+    runner.x=150;
+  }
+
+  if(keyWentDown("space")&& runner.y >= 100) {
+    runner.velocityY = -12;
+}
+
+//add gravity
+runner.velocityY = runner.velocityY + 0.8
+
+  for(var i = 0;i < maxSnow; i++){
+    ice[i].display();
+    ice[i].changePosition();
+    }    
+    
+
+
+ground.display();
+
   
-   for (var i = 0; i < plinkos.length; i++) {
-     
-     plinkos[i].display();
-     
-   }
-   if(frameCount%60===0){
-     particles.push(new particle(random(width/2-30, width/2+30), 10,10));
-     score++;
-   }
- 
-  for (var j = 0; j < particles.length; j++) {
-   
-     particles[j].display();
-   }
-   for (var k = 0; k < divisions.length; k++) {
-     
-     divisions[k].display();
-   }
+  drawSprites();
+
 }
